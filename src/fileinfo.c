1ee79eb2fa579fa5e6bd20685115796454f558b4
tt67wq <807643203@qq.com>
2018-01-06 14:45:33 +0800

    添加了文件信息和简单ls

---
+++ src/fileinfo.c
@@ -0,0 +1,51 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  fileinfo.c
+ *
+ *    Description:  读取文件信息
+ *
+ *        Version:  1.0
+ *        Created:  2018-01-06
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  wanqiang
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#define LENGTH(a) ((sizeof(a)) / (sizeof(a[0])))
+#define TRUE 1
+#define FALSE 0
+
+void show_stat_info(char *fname, struct stat *buf);
+
+int main(int ac, char *av[]) {
+  struct stat info;
+
+  if (ac > 1) {
+    if (stat(av[1], &info) != -1) {
+      show_stat_info(av[1], &info);
+      return 0;
+    } else {
+      perror(av[1]);
+    }
+    return 1;
+  }
+}
+
+void show_stat_info(char *fname, struct stat *buf) {
+  printf("  mode: %o\n", buf->st_mode);
+  printf("   links: %d\n", buf->st_nlink);
+  printf("    user: %d\n", buf->st_uid);
+  printf("   group: %d\n", buf->st_gid);
+  printf("    size: %d\n", buf->st_size);
+  printf(" modtime: %d\n", buf->st_mtime);
+  printf("    name: %s\n", fname);
+}
---
+++ src/ls1.c
@@ -0,0 +1,51 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  ls1.c
+ *
+ *    Description:  ls的简单实现
+ *
+ *        Version:  1.0
+ *        Created:  2018-01-06
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  wanqiang
+ *
+ * =====================================================================================
+ */
+
+#include <dirent.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+
+#define LENGTH(a) ((sizeof(a)) / (sizeof(a[0])))
+#define TRUE 1
+#define FALSE 0
+
+void do_ls(char[]);
+
+int main(int ac, char *av[]) {
+  if (ac == 1)
+    do_ls(".");
+  else
+    while (--ac) {
+      printf("%s:\n", *++av);
+      do_ls(*av);
+    }
+  return 0;
+}
+
+void do_ls(char dirname[]) {
+  DIR *dir_ptr;
+  struct dirent *direntp;
+
+  if ((dir_ptr = opendir(dirname)) == NULL)
+    fprintf(stderr, "ls1, cannot open %s\n", dirname);
+  else {
+    while ((direntp = readdir(dir_ptr)) != NULL)
+      printf("%s\n", direntp->d_name);
+    closedir(dir_ptr);
+  }
+}
