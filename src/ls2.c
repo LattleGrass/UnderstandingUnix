e0ccdfd8630783430071692b1a61a6e60c293479
tt67wq <807643203@qq.com>
2018-01-10 17:15:54 +0800

    ls -l 完成

--- src/ls2.c
+++ src/ls2.c
@@ -16,6 +16,8 @@
  */
 
 #include <dirent.h>
+#include <grp.h>
+#include <pwd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -31,7 +33,7 @@ void dostat(char *);
 void show_file_info(char *, struct stat *);
 void mode_to_letters(int, char[]);
 char *uid_to_name(uid_t);
-char *gid_to_name(git_t);
+char *gid_to_name(gid_t);
 
 int main(int ac, char **av) {
   if (ac == 1)
@@ -59,6 +61,7 @@ void dostat(char *filename) {
   struct stat info;
 
   if (stat(filename, &info) == -1) {
+    /* printf("fuck!\n"); */
     perror(filename);
   } else {
     show_file_info(filename, &info);
@@ -72,16 +75,16 @@ void show_file_info(char *filename, struct stat *info_p) {
 
   mode_to_letters(info_p->st_mode, modestr);
   printf("%s", modestr);
-  printf("%4s", (int)info_p->st_nlink);
-  printf("%-8s", uid_to_name(info_p->st_uid));
-  printf("%-8s", gid_to_name(info_p->st_gid));
-  printf("%8ld", (long)info_p->st_size);
-  printf("%.12s", 4 + ctime(&info_p->st_mtime));
+  printf("%4d ", (int)info_p->st_nlink);
+  printf("%-8s ", uid_to_name(info_p->st_uid));
+  printf("%-8s ", gid_to_name(info_p->st_gid));
+  printf("%8ld ", (long)info_p->st_size);
+  printf("%.12s ", 4 + ctime(&info_p->st_mtime));
   printf("%s\n", filename);
 }
 
 void mode_to_letters(int mode, char *str) {
-  strcpy(str, "------------");
+  strcpy(str, "----------");
   if (S_ISDIR(mode))
     str[0] = 'd';
   if (S_ISCHR(mode))
@@ -103,7 +106,30 @@ void mode_to_letters(int mode, char *str) {
   if (mode & S_IROTH)
     str[7] = 'r';
   if (mode & S_IWOTH)
-    str[8] = "w";
+    str[8] = 'w';
   if (mode & S_IXOTH)
     str[9] = 'x';
+}
+
+char *uid_to_name(uid_t uid) {
+  struct passwd *getpwuid(), *pw_ptr;
+  static char numstr[10];
+  if ((pw_ptr = getpwuid(uid)) == NULL) {
+    sprintf(numstr, "%d", uid);
+    return numstr;
+  } else {
+    return pw_ptr->pw_name;
+  }
+}
+
+char *gid_to_name(gid_t gid) {
+  struct group *getgrgid(), *grp_ptr;
+  static char numstr[10];
+
+  if ((grp_ptr = getgrgid(gid)) == NULL) {
+    sprintf(numstr, "%d", gid);
+    return numstr;
+  } else {
+    return grp_ptr->gr_name;
+  }
 }
