bc95dbbcae72452c460f48bbfa264155b13b9119
tt67wq <807643203@qq.com>
2018-02-06 13:32:16 +0800

    debug

--- src/play_again2.c
+++ src/play_again2.c
@@ -27,6 +27,7 @@
 
 void set_crmode(void);
 int get_response(char *);
+void tty_mode(int);
 
 int main() {
         int response;
@@ -35,4 +36,38 @@ int main() {
         response = get_response(QUESTION);
         tty_mode(1);
         return response;
+}
+
+int get_response(char *question) {
+        int input;
+        printf("%s(y/n)?", question);
+        while (1) {
+                switch (input = getchar()) {
+                        case 'y':
+                        case 'Y':
+                                return 0;
+                        case 'n':
+                        case 'N':
+                                return 1;
+                        default:
+                                printf("\ncannot understand %c, ", input);
+                                printf("please type y or n\n");
+                }
+        }
+}
+
+void set_crmode() {
+        struct termios ttystate;
+        tcgetattr(0, &ttystate);
+        ttystate.c_cflag &= ~ICANON;
+        ttystate.c_cc[VMIN] = 1;
+        tcsetattr(0, TCSANOW, &ttystate);
+}
+
+void tty_mode(int how) {
+        static struct termios original_mode;
+        if (how == 0)
+                tcgetattr(0, &original_mode);
+        else
+                tcsetattr(0, TCSANOW, &original_mode);
 }
