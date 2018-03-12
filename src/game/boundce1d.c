f815b0b534bedd72116c0b19060c6882b1b6bb32
tt67wq <807643203@qq.com>
2018-03-12 11:29:12 +0800

    代码调试成功

--- src/game/boundce1d.c
+++ src/game/boundce1d.c
@@ -8,7 +8,7 @@
  *        Version:  1.0
  *        Created:  2018-03-12
  *       Revision:  none
- *       Compiler:  gcc
+ *       Compiler:  gcc boundce1d.c -l curses -o boundce1d
  *
  *         Author:  wanqiang
  *
@@ -82,4 +82,18 @@ void move_msg(int signum) {
                 dir = 1;
         else if (dir == 1 && col + strlen(MESSAGE) >= COLS)
                 dir = -1;
+}
+
+int set_ticker(int n_msecs) {
+        struct itimerval new_timeset;
+        long n_sec, n_usecs;
+
+        n_sec = n_msecs / 1000;
+        n_usecs = (n_msecs % 1000) * 1000L;
+        new_timeset.it_interval.tv_sec = n_sec;
+        new_timeset.it_interval.tv_usec = n_usecs;
+
+        new_timeset.it_value.tv_sec = n_sec;
+        new_timeset.it_value.tv_usec = n_usecs;
+        return setitimer(ITIMER_REAL, &new_timeset, NULL);
 }
