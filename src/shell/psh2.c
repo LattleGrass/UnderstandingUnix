c936992f074d12a682771eb2f3b0157367dc591a
tt67wq <807643203@qq.com>
2018-03-31 11:19:30 +0800

    shell第二版本

---
+++ src/shell/psh2.c
@@ -0,0 +1,81 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  psh2.c
+ *
+ *    Description:  shell
+ *
+ *        Version:  2.0
+ *        Created:  2018-03-31
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  wanqiang
+ *
+ * =====================================================================================
+ */
+
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define LENGTH(a) ((sizeof(a)) / (sizeof(a[0])))
+#define TRUE 1
+#define FALSE 0
+#define MAXARGS 20
+#define ARGLEN 100
+
+int main() {
+        char *arglist[MAXARGS + 1];
+        int numargs;
+        char argbuf[ARGLEN];
+        char *makestring();
+        int execute(char *[]);
+        numargs = 0;
+        while (numargs < MAXARGS) {
+                printf("Arg[%d]? ", numargs);
+                if (fgets(argbuf, ARGLEN, stdin) && *argbuf != '\n')
+                        arglist[numargs++] = makestring(argbuf);
+                else {
+                        if (numargs > 0) {
+                                arglist[numargs] = NULL;
+                                execute(arglist);
+                                numargs = 0;
+                        }
+                }
+        }
+        return 0;
+}
+
+int execute(char *arglist[]) {
+        int pid, exitstatus;
+        pid = fork();
+        switch (pid) {
+                case -1:
+                        perror("fork failed");
+                        exit(1);
+                case 0:
+                        execvp(arglist[0], arglist);
+                        perror("execvp failed");
+                        exit(1);
+                default:
+                        while (wait(&exitstatus) != pid)
+                                ;
+                        printf("child exited with status %d, %d\n",
+                               exitstatus >> 8, exitstatus & 0377);
+        }
+}
+
+char *makestring(char *buf) {
+        char *cp;
+        buf[strlen(buf) - 1] = '\0';
+        cp = malloc(strlen(buf) + 1);
+        if (cp == NULL) {
+                fprintf(stderr, "no memory\n");
+                exit(1);
+        }
+        strcpy(cp, buf);
+        return cp;
+}
