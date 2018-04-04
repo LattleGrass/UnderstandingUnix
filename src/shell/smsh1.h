8b0b42d4919d5b5c9bebadc5e36b3c3510164e95
tt67wq <807643203@qq.com>
2018-04-04 14:35:48 +0800

    small shell复杂shell版本

---
+++ src/shell/smsh1.h
@@ -0,0 +1,17 @@
+#ifndef _SMSH_H_
+#define _SMSH_H_
+
+#define YES 1
+#define NO 0
+
+#include <stddef.h>
+
+char *next_cmd();
+char **splitline(char *);
+void freelist(char **);
+void *emalloc(size_t);
+void *erealloc(void *, size_t);
+int execute(char **);
+void fatal(char *, char *, int);
+
+#endif
