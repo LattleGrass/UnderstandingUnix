862febaab885f6a9e2aa831e604a449d24352666
tt67wq <807643203@qq.com>
2018-04-08 20:06:00 +0800

    smsh2版本

---
+++ src/shell/process.c
@@ -0,0 +1,39 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  process.c
+ *
+ *    Description:
+ *
+ *        Version:  1.0
+ *        Created:  2018-04-08
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  wanqiang
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "smsh2.h"
+
+#define LENGTH(a) ((sizeof(a)) / (sizeof(a[0])))
+#define TRUE 1
+#define FALSE 0
+
+int is_control_command(char *);
+int do_control_command(char **);
+int ok_to_execute();
+
+int process(char **args) {
+        int rv = 0;
+        if (args[0] == NULL)
+                rv = 0;
+        else if (is_control_command(args[0]))
+                rv = do_control_command(args);
+        else if (ok_to_execute())
+                rv = execute(args);
+        return rv;
+}
