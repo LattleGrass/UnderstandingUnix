f5bfb451440ae6e48ae369782f34f608e819fcf2
tt67wq <807643203@qq.com>
2018-04-19 11:00:38 +0800

    编写了简单的socket客户端

--- src/cp1.c
+++ src/cp1.c
@@ -28,40 +28,42 @@
 void oops(char *, char *);
 
 int main(int ac, char *av[]) {
-	int in_fd, out_fd, n_char;
-	char buf[BUFFSIZE];
+        int in_fd, out_fd, n_char;
+        char buf[BUFFSIZE];
 
-	if (ac != 3) {
-		fprintf(stderr, "usage: %s source destination\n", *av);
-		exit(1);
-	}
+        if (ac != 3) {
+                fprintf(stderr, "usage: %s source destination\n", *av);
+                exit(1);
+        }
 
-	if ((in_fd = open(av[1], O_RDONLY)) == -1) // 打开文件，获得文件描述符
-		oops("Cannot open ", av[1]);
+        if ((in_fd = open(av[1], O_RDONLY)) == -1)  // 打开文件，获得文件描述符
+                oops("Cannot open ", av[1]);
 
-	if ((out_fd = creat(av[2], COPYMODE)) == -1) { // 创建要复制的文件，获得文件描述符
-		oops("Cannot creat ", av[2]);
-	}
+        if ((out_fd = creat(av[2], COPYMODE)) ==
+            -1) {  // 创建要复制的文件，获得文件描述符
+                oops("Cannot creat ", av[2]);
+        }
 
-	while ((n_char = read(in_fd, buf, BUFFSIZE)) > 0) { // 从源文件中读出buffsize长度的内容
-		if (write(out_fd, buf, n_char) != n_char) { // 写入目标文件中
-			oops("write error to ", av[2]);
-		}
-	}
+        while ((n_char = read(in_fd, buf, BUFFSIZE)) >
+               0) {  // 从源文件中读出buffsize长度的内容
+                if (write(out_fd, buf, n_char) != n_char) {  // 写入目标文件中
+                        oops("write error to ", av[2]);
+                }
+        }
 
-	/* printf("n_char: %d\n", n_char); */
+        /* printf("n_char: %d\n", n_char); */
 
-	if (n_char == -1) {
-		oops("read error from ", av[1]);
-	}
+        if (n_char == -1) {
+                oops("read error from ", av[1]);
+        }
 
-	if (close(in_fd) == -1 || close(out_fd) == -1) { // 关闭两个文件描述符
-		oops("Error closing files", "");
-	}
+        if (close(in_fd) == -1 || close(out_fd) == -1) {  // 关闭两个文件描述符
+                oops("Error closing files", "");
+        }
 }
 
 void oops(char *s1, char *s2) {
-	fprintf(stderr, "Error: %s", s1);
-	perror(s2);
-	exit(1);
+        fprintf(stderr, "Error: %s", s1);
+        perror(s2);
+        exit(1);
 }
--- src/timeclnt.c
+++ src/timeclnt.c
@@ -19,6 +19,7 @@
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 
@@ -37,4 +38,32 @@ int main(int ac, char **av) {
         int sock_id, sock_fd;
         char message[BUFSIZ];
         int messlen;
+
+        /* step 1: Get a socket */
+        sock_id = socket(AF_INET, SOCK_STREAM, 0);
+        if (sock_id == -1)
+                oops("socket");
+
+        /* step 2: connect to a server */
+        bzero(&servadd, sizeof(servadd));
+
+        hp = gethostbyname(av[1]);
+        if (hp == NULL)
+                oops(av[1]);
+        bcopy(hp->h_addr, (struct sockaddr *)&servadd.sin_addr, hp->h_length);
+
+        servadd.sin_port = htons(atoi(av[2]));
+        servadd.sin_family = AF_INET;
+
+        if (connect(sock_id, (struct sockaddr *)&servadd, sizeof(servadd)) != 0)
+                oops("connect");
+
+        /* step3: transfer data from server, then hangup */
+        messlen = read(sock_id, message, BUFSIZ);
+        if (messlen == -1)
+                oops("read");
+        if (write(1, message, messlen) != messlen)
+                oops("write");
+        close(sock_id);
+        exit(0);
 }
