4764b9090622405c71fde9b3167f04b3f784440b
tt67wq <807643203@qq.com>
2018-04-24 18:37:13 +0800

    oopsie B|

---
+++ src/thread/hello_multi.c
@@ -0,0 +1,49 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  hello_multi.c
+ *
+ *    Description:  多线程
+ *
+ *        Version:  1.0
+ *        Created:  2018-04-24
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  wanqiang
+ *
+ * =====================================================================================
+ */
+
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define LENGTH(a) ((sizeof(a)) / (sizeof(a[0])))
+#define TRUE 1
+#define FALSE 0
+#define NUM 5
+
+int main() {
+        pthread_t t1, t2;
+
+        void *print_msg(void *);
+
+        pthread_create(&t1, NULL, print_msg, (void *)"hello");
+        pthread_create(&t2, NULL, print_msg, (void *)"world");
+
+        pthread_join(t1, NULL);
+        pthread_join(t2, NULL);
+}
+
+void *print_msg(void *m) {
+        char *cp = (char *)m;
+        int i;
+        for (i = 0; i < NUM; i++) {
+                printf("%s ", m);
+                fflush(stdout);
+                sleep(1);
+        }
+        return NULL;
+}
