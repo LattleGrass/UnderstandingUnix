f778942fc332e8e3305b097379ea4859be3f3493
tt67wq <807643203@qq.com>
2018-04-28 14:05:18 +0800

    I know what I am doing. Trust me.

---
+++ src/IPC/shm_tc2.c
@@ -0,0 +1,90 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  shm_tc2.c
+ *
+ *    Description:  时间服务客户端信号量版本
+ *
+ *        Version:  1.0
+ *        Created:  2018-04-28
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  wanqiang
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/shm.h>
+#include <sys/types.h>
+#include <time.h>
+
+#define LENGTH(a) ((sizeof(a)) / (sizeof(a[0])))
+#define TRUE 1
+#define FALSE 0
+#define TIME_MEM_KEY 99
+#define TIME_SEM_KEY 9900
+#define SEG_SIZE ((size_t)100)
+#define oops(m, x)                                                             \
+        {                                                                      \
+                perror(m);                                                     \
+                exit(x);                                                       \
+        }
+
+int seg_id, semset_id;
+void wait_and_lock(int);
+void release_lock(int);
+
+int main() {
+        int seg_id;
+        char *mem_ptr, *ctime();
+        long now;
+
+        int semset_id;
+
+        /* create a shared memory segment */
+        seg_id = shmget(TIME_MEM_KEY, SEG_SIZE, 0777);
+        if (seg_id == -1)
+                oops("shmget", 1);
+
+        /* attach to it and get a pointer to where it attaches */
+        mem_ptr = shmat(seg_id, NULL, 0);
+        if (mem_ptr == (void *)-1)
+                oops("shmget", 2);
+
+        /* connect to semaphore set 9900 with 2 semaphores */
+        semset_id = semget(TIME_SEM_KEY, 2, 0);
+        wait_and_lock(semset_id);
+        printf("The time, direct from memory:..%s", mem_ptr);
+
+        release_lock(semset_id);
+        shmdt(mem_ptr);
+}
+
+void wait_and_lock(int semset_id) {
+        struct sembuf actions[2];
+        actions[0].sem_num = 0;
+        actions[0].sem_flg = SEM_UNDO;
+        actions[0].sem_op = 0;
+
+        actions[1].sem_num = 1;
+        actions[1].sem_flg = SEM_UNDO;
+        actions[1].sem_op = +1;
+
+        if (semop(semset_id, actions, 2) == -1)
+                oops("semop: locking", 10);
+}
+
+void release_lock(int semset_id) {
+        struct sembuf actions[1];
+        actions[0].sem_num = 1;
+        actions[0].sem_flg = SEM_UNDO;
+        actions[0].sem_op = -1;
+
+        if (semop(semset_id, actions, 1) == -1)
+                oops("semop: unlocking", 10);
+}
